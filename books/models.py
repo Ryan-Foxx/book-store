1fcd5ff8ebb825c3d6df75514bc4b0b85bd0f6d7
Ryan Fox <foxr8326@gmail.com>
2025-10-02 00:54:53 +0330

    design ContentFormat model

--- books/models.py
+++ books/models.py
@@ -42,3 +42,10 @@ class Language(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class ContentFormat(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+
+    def __str__(self):
+        return self.name
