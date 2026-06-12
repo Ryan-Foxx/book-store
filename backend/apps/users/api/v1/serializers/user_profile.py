4ff5884902ae10acfb96fd6a9c7e5fd1586448d1
Ryan Fox <foxr8326@gmail.com>
2026-06-12 18:37:35 +0330

    feat(api): add user update support

--- backend/apps/users/api/v1/serializers/user_profile.py
+++ backend/apps/users/api/v1/serializers/user_profile.py
@@ -9,14 +9,25 @@ class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ("id", "email", "phone_number", "username", "first_name", "last_name", "role")
+        read_only_fields = ("id", "email", "phone_number", "role")
 
 
 class UserProfileSerializer(serializers.ModelSerializer):
 
     # Nested Serializer Fields
-    user = UserSerializer(read_only=True)
+    user = UserSerializer()
 
     class Meta:
         model = UserProfile
         fields = ("id", "user", "avatar")
         read_only_fields = ("id",)
+
+    def update(self, instance, validated_data):
+        user_data = validated_data.pop("user", {})
+
+        if user_data:
+            user_serializer = UserSerializer(instance.user, data=user_data, partial=self.partial)
+            user_serializer.is_valid(raise_exception=True)
+            user_serializer.save()
+
+        return super().update(instance, validated_data)
