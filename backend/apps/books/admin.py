59bf74705aef0b7953d7458fa8842276e83f992a
Ryan Fox <foxr8326@gmail.com>
2026-07-26 20:48:24 +0330

    feat(admin): register author model to django admin panel

--- backend/apps/books/admin.py
+++ backend/apps/books/admin.py
@@ -1,3 +1,27 @@
+from apps.books.models import Author
 from django.contrib import admin
 
+
 # Register your models here.
+@admin.register(Author)
+class AuthorAdmin(admin.ModelAdmin):
+    model = Author
+
+    list_display = (
+        "id",
+        "name",
+        "avatar",
+        "about",
+        "biography",
+        "created_at",
+        "modified_at",
+    )
+
+    list_filter = (
+        "created_at",
+        "modified_at",
+    )
+
+    list_per_page = 10
+    search_fields = ("name",)
+    ordering = ("-created_at",)
