c80f00943c8c98d6f00166fecab7756187cba466
Ryan Fox <foxr8326@gmail.com>
2026-08-04 00:22:35 +0330

    feat(api): create pagination for admin author api

--- backend/apps/books/api/v1/admin/views/author_viewset.py
+++ backend/apps/books/api/v1/admin/views/author_viewset.py
@@ -1,3 +1,4 @@
+from apps.books.api.v1.admin.pagination.author_pagination import AuthorPagination
 from apps.books.api.v1.admin.serializers.author_serializers import AuthorSerializer
 from apps.books.selectors.author_selectors import get_admin_author_queryset
 from rest_framework.viewsets import ModelViewSet
@@ -5,5 +6,7 @@ from rest_framework.viewsets import ModelViewSet
 
 # Create your views here.
 class AuthorViewSet(ModelViewSet):
+    pagination_class = AuthorPagination
+
     serializer_class = AuthorSerializer
     queryset = get_admin_author_queryset()
