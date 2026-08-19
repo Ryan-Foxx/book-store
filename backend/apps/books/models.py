6b519ee83d1c21ba6daf46fc2f81a52dd3c82659
Ryan Fox <foxr8326@gmail.com>
2026-08-19 10:25:46 +0330

    feat(models): create award model and migrate to db

--- backend/apps/books/models.py
+++ backend/apps/books/models.py
@@ -13,3 +13,14 @@ class Author(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class Award(models.Model):
+    authors = models.ManyToManyField(Author, blank=True, related_name="awards")
+    title = models.CharField(max_length=255, unique=True)
+    year_received = models.DateField(null=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    modified_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
