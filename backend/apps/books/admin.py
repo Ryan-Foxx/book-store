be85b672438a569a855665ad81e42774bdf4495b
Ryan Fox <foxr8326@gmail.com>
2026-08-19 11:07:42 +0330

    feat(admin): register award model to django admin panel

--- backend/apps/books/admin.py
+++ backend/apps/books/admin.py
@@ -1,4 +1,4 @@
-from apps.books.models import Author
+from apps.books.models import Author, Award
 from django.contrib import admin
 
 
@@ -25,3 +25,27 @@ class AuthorAdmin(admin.ModelAdmin):
     list_per_page = 10
     search_fields = ("name",)
     ordering = ("-created_at",)
+
+
+@admin.register(Award)
+class AwardAdmin(admin.ModelAdmin):
+    model = Award
+
+    list_display = (
+        "id",
+        # "authors",
+        "title",
+        "year_received",
+        "created_at",
+        "modified_at",
+    )
+
+    list_filter = (
+        "created_at",
+        "modified_at",
+    )
+
+    list_per_page = 10
+    search_fields = ("title",)
+    ordering = ("-created_at",)
+    filter_horizontal = ("authors",)
