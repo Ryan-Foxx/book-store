f2dd5d1c2d044690c9dd622c19e38634f634a99e
Ryan Fox <foxr8326@gmail.com>
2026-08-22 16:47:46 +0330

    feat(api): add permission for admin award api

--- backend/apps/books/api/v1/admin/views/award_viewset.py
+++ backend/apps/books/api/v1/admin/views/award_viewset.py
@@ -1,14 +1,17 @@
 from apps.books.api.v1.admin.filters.award_filters import AwardFilter
 from apps.books.api.v1.admin.pagination.award_pagination import AwardPagination
+from apps.books.api.v1.admin.permissions.award_permissions import IsAdmin
 from apps.books.api.v1.admin.serializers.award_serializers import (
     AwardReadSerializer,
     AwardSerializer,
 )
 from apps.books.selectors.award_selectors import get_admin_award_queryset
+from core.permissions import IsOwner
 from rest_framework.viewsets import ModelViewSet
 
 
 class AwardViewSet(ModelViewSet):
+    permission_classes = [IsOwner | IsAdmin]
     pagination_class = AwardPagination
     filterset_class = AwardFilter
 
