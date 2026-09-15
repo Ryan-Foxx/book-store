c5b89b13cb6ba874b0679d1c10cb130ac60fd0e7
Ryan Fox <foxr8326@gmail.com>
2026-09-15 11:06:21 +0330

    feat(api): add search and ordering for admin translator api

--- backend/apps/books/api/v1/admin/views/translator_viewset.py
+++ backend/apps/books/api/v1/admin/views/translator_viewset.py
@@ -1,3 +1,4 @@
+from apps.books.api.v1.admin.filters.translator_filters import TranslatorFilter
 from apps.books.api.v1.admin.pagination.translator_pagination import (
     TranslatorPagination,
 )
@@ -10,6 +11,7 @@ from rest_framework.viewsets import ModelViewSet
 
 class TranslatorViewSet(ModelViewSet):
     pagination_class = TranslatorPagination
+    filterset_class = TranslatorFilter
 
     serializer_class = TranslatorSerializer
     queryset = get_admin_translator_queryset()
