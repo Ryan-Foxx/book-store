73fd2c1710156144f270dd3c6aecb857f56e4d1c
Ryan Fox <foxr8326@gmail.com>
2026-09-15 11:16:27 +0330

    feat(api): add permission for admin translator api

--- backend/apps/books/api/v1/admin/views/translator_viewset.py
+++ backend/apps/books/api/v1/admin/views/translator_viewset.py
@@ -2,14 +2,17 @@ from apps.books.api.v1.admin.filters.translator_filters import TranslatorFilter
 from apps.books.api.v1.admin.pagination.translator_pagination import (
     TranslatorPagination,
 )
+from apps.books.api.v1.admin.permissions.translator_permissions import IsAdmin
 from apps.books.api.v1.admin.serializers.translator_serializers import (
     TranslatorSerializer,
 )
 from apps.books.selectors.translator_selectors import get_admin_translator_queryset
+from core.permissions import IsOwner
 from rest_framework.viewsets import ModelViewSet
 
 
 class TranslatorViewSet(ModelViewSet):
+    permission_classes = [IsOwner | IsAdmin]
     pagination_class = TranslatorPagination
     filterset_class = TranslatorFilter
 
