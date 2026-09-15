5041c26c8a47c766b24ff9472378b3b0554f44f1
Ryan Fox <foxr8326@gmail.com>
2026-09-15 11:01:10 +0330

    feat(api): create pagination for admin translator api

--- backend/apps/books/api/v1/admin/views/translator_viewset.py
+++ backend/apps/books/api/v1/admin/views/translator_viewset.py
@@ -1,3 +1,6 @@
+from apps.books.api.v1.admin.pagination.translator_pagination import (
+    TranslatorPagination,
+)
 from apps.books.api.v1.admin.serializers.translator_serializers import (
     TranslatorSerializer,
 )
@@ -6,5 +9,7 @@ from rest_framework.viewsets import ModelViewSet
 
 
 class TranslatorViewSet(ModelViewSet):
+    pagination_class = TranslatorPagination
+
     serializer_class = TranslatorSerializer
     queryset = get_admin_translator_queryset()
