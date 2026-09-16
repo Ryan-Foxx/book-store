1f6e913dc84d7aaf7623d722714712813a7b2942
Ryan Fox <foxr8326@gmail.com>
2026-09-16 18:29:21 +0330

    feat(api): create pagination for admin category api

--- backend/apps/books/api/v1/admin/views/category_viewset.py
+++ backend/apps/books/api/v1/admin/views/category_viewset.py
@@ -1,8 +1,11 @@
+from apps.books.api.v1.admin.pagination.category_pagination import CategoryPagination
 from apps.books.api.v1.admin.serializers.category_serializers import CategorySerializer
 from apps.books.selectors.category_selectors import get_admin_category_queryset
 from rest_framework.viewsets import ModelViewSet
 
 
 class CategoryViewSet(ModelViewSet):
+    pagination_class = CategoryPagination
+
     serializer_class = CategorySerializer
     queryset = get_admin_category_queryset()
