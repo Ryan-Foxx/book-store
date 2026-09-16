013bcbfc18e41bc175ffb0c7e49947398049c794
Ryan Fox <foxr8326@gmail.com>
2026-09-16 07:42:07 +0330

    feat(api): add search and ordering for admin publisher api

--- backend/apps/books/api/v1/admin/views/publisher_viewset.py
+++ backend/apps/books/api/v1/admin/views/publisher_viewset.py
@@ -1,3 +1,4 @@
+from apps.books.api.v1.admin.filters.publisher_filters import PublisherFilter
 from apps.books.api.v1.admin.pagination.publisher_pagination import PublisherPagination
 from apps.books.api.v1.admin.serializers.publisher_serializers import (
     PublisherSerializer,
@@ -8,6 +9,7 @@ from rest_framework.viewsets import ModelViewSet
 
 class PublisherViewSet(ModelViewSet):
     pagination_class = PublisherPagination
+    filterset_class = PublisherFilter
 
     serializer_class = PublisherSerializer
     queryset = get_admin_publisher_queryset()
