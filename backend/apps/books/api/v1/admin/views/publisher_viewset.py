ad715eea8915e6879068a103d6d346ff6ef89089
Ryan Fox <foxr8326@gmail.com>
2026-09-16 07:50:00 +0330

    feat(api): add permission for admin publisher api

--- backend/apps/books/api/v1/admin/views/publisher_viewset.py
+++ backend/apps/books/api/v1/admin/views/publisher_viewset.py
@@ -1,13 +1,16 @@
 from apps.books.api.v1.admin.filters.publisher_filters import PublisherFilter
 from apps.books.api.v1.admin.pagination.publisher_pagination import PublisherPagination
+from apps.books.api.v1.admin.permissions.publisher_permissions import IsAdmin
 from apps.books.api.v1.admin.serializers.publisher_serializers import (
     PublisherSerializer,
 )
 from apps.books.selectors.publisher_selectors import get_admin_publisher_queryset
+from core.permissions import IsOwner
 from rest_framework.viewsets import ModelViewSet
 
 
 class PublisherViewSet(ModelViewSet):
+    permission_classes = [IsOwner | IsAdmin]
     pagination_class = PublisherPagination
     filterset_class = PublisherFilter
 
