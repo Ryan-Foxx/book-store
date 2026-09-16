a20a9548241bc388211eb571e0427b67e4a8fb85
Ryan Fox <foxr8326@gmail.com>
2026-09-16 18:32:53 +0330

    feat(api): add search and ordering for admin category api

--- backend/apps/books/api/v1/admin/views/category_viewset.py
+++ backend/apps/books/api/v1/admin/views/category_viewset.py
@@ -1,3 +1,4 @@
+from apps.books.api.v1.admin.filters.category_filters import CategoryFilter
 from apps.books.api.v1.admin.pagination.category_pagination import CategoryPagination
 from apps.books.api.v1.admin.serializers.category_serializers import CategorySerializer
 from apps.books.selectors.category_selectors import get_admin_category_queryset
@@ -6,6 +7,7 @@ from rest_framework.viewsets import ModelViewSet
 
 class CategoryViewSet(ModelViewSet):
     pagination_class = CategoryPagination
+    filterset_class = CategoryFilter
 
     serializer_class = CategorySerializer
     queryset = get_admin_category_queryset()
