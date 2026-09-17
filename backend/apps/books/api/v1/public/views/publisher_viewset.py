de12de4bfcc318463be34c07a8c2836b58547bef
Ryan Fox <foxr8326@gmail.com>
2026-09-17 18:15:39 +0330

    feat(api): create pagination for public publisher api

--- backend/apps/books/api/v1/public/views/publisher_viewset.py
+++ backend/apps/books/api/v1/public/views/publisher_viewset.py
@@ -1,3 +1,4 @@
+from apps.books.api.v1.public.pagination.publisher_pagination import PublisherPagination
 from apps.books.api.v1.public.serializers.publisher_serializers import (
     PublisherDetailSerializer,
     PublisherListSerializer,
@@ -11,6 +12,8 @@ from rest_framework.viewsets import ReadOnlyModelViewSet
 
 # Create your views here.
 class PublisherViewSet(ReadOnlyModelViewSet):
+    pagination_class = PublisherPagination
+
     def get_serializer_class(self):
         if self.action == "list":
             return PublisherListSerializer
