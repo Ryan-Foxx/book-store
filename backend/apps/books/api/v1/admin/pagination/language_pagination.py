dcbd09e2cb2b873586edc868f735d4e2dc4a3ca6
Ryan Fox <foxr8326@gmail.com>
2026-09-17 12:15:53 +0330

    feat(api): create pagination for admin language api

---
+++ backend/apps/books/api/v1/admin/pagination/language_pagination.py
@@ -0,0 +1,5 @@
+from core.pagination import BasePagination
+
+
+class LanguagePagination(BasePagination):
+    page_size = 20
--- backend/apps/books/api/v1/admin/views/language_viewset.py
+++ backend/apps/books/api/v1/admin/views/language_viewset.py
@@ -1,8 +1,11 @@
+from apps.books.api.v1.admin.pagination.language_pagination import LanguagePagination
 from apps.books.api.v1.admin.serializers.language_serializers import LanguageSerializer
 from apps.books.selectors.language_selectors import get_admin_language_queryset
 from rest_framework.viewsets import ModelViewSet
 
 
 class LanguageViewSet(ModelViewSet):
+    pagination_class = LanguagePagination
+
     serializer_class = LanguageSerializer
     queryset = get_admin_language_queryset()
