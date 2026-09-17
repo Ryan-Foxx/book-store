868cf8c560bfe72fe0c22af14e37ea14673cfedf
Ryan Fox <foxr8326@gmail.com>
2026-09-17 12:18:21 +0330

    feat(api): add search and ordering for admin language api

--- backend/apps/books/api/v1/admin/views/language_viewset.py
+++ backend/apps/books/api/v1/admin/views/language_viewset.py
@@ -1,3 +1,4 @@
+from apps.books.api.v1.admin.filters.language_filters import LanguageFilter
 from apps.books.api.v1.admin.pagination.language_pagination import LanguagePagination
 from apps.books.api.v1.admin.serializers.language_serializers import LanguageSerializer
 from apps.books.selectors.language_selectors import get_admin_language_queryset
@@ -6,6 +7,7 @@ from rest_framework.viewsets import ModelViewSet
 
 class LanguageViewSet(ModelViewSet):
     pagination_class = LanguagePagination
+    filterset_class = LanguageFilter
 
     serializer_class = LanguageSerializer
     queryset = get_admin_language_queryset()
