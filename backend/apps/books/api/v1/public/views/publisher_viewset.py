7d601d30d44a0586843ba80feebc64a57ba768fd
Ryan Fox <foxr8326@gmail.com>
2026-09-17 18:18:25 +0330

    feat(api): add search for public publisher api

--- backend/apps/books/api/v1/public/views/publisher_viewset.py
+++ backend/apps/books/api/v1/public/views/publisher_viewset.py
@@ -1,3 +1,4 @@
+from apps.books.api.v1.public.filters.publisher_filters import PublisherFilter
 from apps.books.api.v1.public.pagination.publisher_pagination import PublisherPagination
 from apps.books.api.v1.public.serializers.publisher_serializers import (
     PublisherDetailSerializer,
@@ -13,6 +14,7 @@ from rest_framework.viewsets import ReadOnlyModelViewSet
 # Create your views here.
 class PublisherViewSet(ReadOnlyModelViewSet):
     pagination_class = PublisherPagination
+    filterset_class = PublisherFilter
 
     def get_serializer_class(self):
         if self.action == "list":
