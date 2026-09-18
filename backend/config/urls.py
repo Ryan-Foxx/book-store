140a81f59f4076a0ad1345eb01646ef927a6214f
Ryan Fox <foxr8326@gmail.com>
2026-09-18 19:51:35 +0330

    chore(packages): install drf_spectacular and setup config

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -19,6 +19,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
+from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -27,6 +28,10 @@ urlpatterns = [
     path("api/v1/", include("apps.users.api.v1.urls")),
     
     path("api/v1/", include("api.v1.urls")),
+    
+    # Api Documentation
+    path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
+    path('api/docs/swagger/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
 
 ] + debug_toolbar_urls()
 
